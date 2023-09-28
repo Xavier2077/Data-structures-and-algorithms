04d1a0cd83db35d16ba9645a421616e0d874ec24
Xavier <102642232+Xavier2077@users.noreply.github.com>
2023-09-28 17:34:30 +0800

    Create merge_sort.cpp

---
+++ 排序与查找/merge_sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAXN = 1000;
+int arr[MAXN];
+int n;
+
+void merg_sort(int s, int e)
+{
+	if(s >= e) return;
+	int mid = (s + e) >> 1;
+	merg_sort(s, mid);
+	merg_sort(mid + 1, e);
+	int tmp[MAXN];
+	int l = s, r = mid + 1, k = s;
+	while(l <= mid || r <= e)
+	{
+		if(r > e || (l <= mid && arr[l] <= arr[r]) )
+		{
+			tmp[k++] = arr[l++];
+		}
+		else
+		{
+			tmp[k++] = arr[r++];
+		}
+	}
+	for(int i = s;i <= e;++i)
+	{
+		arr[i] = tmp[i];
+	}
+	return;
+}
+
+int main()
+{
+	scanf("%d", &n);
+	for(int i = 0;i < n;++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+	merg_sort(0, n - 1);
+	for(int i = 0;i < n;++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
